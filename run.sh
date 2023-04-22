d1728bfd99c326bfd2bc89f217b99251fb44dc23
Phillipe Martins <hi@inkasa.dev>
2023-04-21 23:30:21 -0300

    initial commit

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Automatically exit with an error code if any command fails
+set -e
+
+# Install package.json dependencies
+npm
+
+# Run project
+npm run build
+
+echo
+echo ========================================================
+echo
+
+# Print commands before running them, to make CI output easier to understand
+set -v
